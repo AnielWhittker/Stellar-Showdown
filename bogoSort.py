0acaea85d133a9ca3147f16d1026413f71f31147
Maxwell Evans <116737758+ToTheMax24@users.noreply.github.com>
2023-08-02 13:19:34 -0400

    Add files via upload
    Signed-off-by: Maxwell Evans <116737758+ToTheMax24@users.noreply.github.com>

---
+++ bogoSort.py
@@ -0,0 +1,32 @@
+import random
+
+def checkForTemp(list):
+    for i in range(len(list) - 1):
+        if list[i].temperature > list[i + 1].temperature:
+            return False
+
+    return True
+
+def checkForDistance(list):
+    for i in range(len(list) - 1):
+        if list[i].distance > list[i + 1].temperature:
+            return False
+
+    return True
+
+def bogoSortTemp(list):
+    returnList = list[:]
+
+    while(checkForTemp(returnList) == False):
+        random.shuffle(list)
+
+    return returnList
+
+
+def bogoSortDistance(list):
+    returnList = list[:]
+
+    while(checkForDistance(returnList) == False):
+        random.shuffle(list)
+
+    return returnList
