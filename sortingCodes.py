9d1cfe857b989ee750ee9890d0aa6d5231087289
AnielWhittker <whittkermelian.a@ufl.edu>
2023-08-02 14:10:17 -0400

    added sorting codes

--- sortingCodes.py
+++ sortingCodes.py
@@ -43,3 +43,113 @@ selectionSort = '''def selectionSort(list):
         list[minIndx] = list[i]
 
     return list'''
+
+# heap sort algorithm code snippet
+heapSort = '''def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[i] < arr[left]:
+        largest = left
+
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+
+    if largest != i:
+        (arr[i], arr[largest]) = (arr[largest], arr[i])
+        heapify(arr, n, largest)
+
+
+def heapSort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        (arr[i], arr[0]) = (arr[0], arr[i])
+        heapify(arr, i, 0)'''
+
+# shell sort algorithm code snippet
+shellSort = '''def shellSort(list):
+    end = len(list)
+    gap = end // 2
+    returnList = list[:]
+
+    while gap > 0:
+        for i in range(gap, end):
+            temp = returnList[i]
+
+            j = i
+
+            while j > gap and returnList[j - gap] > temp:
+                returnList[j] = returnList[j - gap]
+
+                j = j - gap
+
+            returnList[j] = temp
+
+        gap = gap // 2
+
+    return returnList'''
+
+# bubble sort algorithm code snippet
+bubbleSort = '''def bubbleSort(list):
+    length = len(list)
+    returnList = list[:]
+    madeSwap = False
+
+    for i in range(length - 1):
+        for j in range(0, length - i - 1):
+            if returnList[j] > returnList[j + 1]:
+                temp = returnList[j]
+                returnList[j] = returnList[j + 1]
+                returnList[j + 1] = temp
+
+                madeSwap = True
+
+        if madeSwap == False:
+            return returnList
+
+    return returnList'''
+
+
+# quick sort algorithm code snippet
+quickSort = '''def quickSort(list):
+    pivot = list[len(list) // 2]
+
+    smallerThanPivot = []
+    equalToPivot = []
+    greaterThanPivot = []
+
+    for i in list:
+        if list[i] < pivot:
+            smallerThanPivot.append(list[i])
+
+        elif list[i] == pivot:
+            equalToPivot.append(list[i])
+
+        elif list[i] > pivot:
+            greaterThanPivot.append(list[i])
+
+    return quickSort(smallerThanPivot) + equalToPivot + quickSort(greaterThanPivot)'''
+
+# bitonic sort algorithm code snippet
+bitonicSort = '''def bitonicMerge(list, low, count, direction):
+    if count > 1:
+        k = count // 2
+        for i in range(low , low + k):
+            if (direction == 1 and list[i] > list[i + k]) or (direction == 0 and list[i] < list[i + k]):
+                temp = list[i]
+                list[i] = list[i + k]
+                list[i + k] = temp
+        bitonicMerge(list, low, k, direction)
+        bitonicMerge(list, low + k, k, direction)
+
+def bitonicSort(list, low, count, direction):
+    if count > 1:
+        k = count // 2
+        bitonicSort(list, low, k, 1)
+        bitonicSort(list, low + k, k, 0)
+        bitonicMerge(list, low, count, direction)'''
